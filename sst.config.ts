9d80db9fa28960e43d6a8777d9636b2fa182cf1b
radiylon <radiylon@gmail.com>
2025-08-16 00:49:18 -0700

    Add test function, run migrations

--- sst.config.ts
+++ sst.config.ts
@@ -7,7 +7,30 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        aws: {
+          profile: input?.stage === "production" ? "radiylon-production" : "radiylon-dev",
+          region: "us-west-1",
+        },
+      },
     };
   },
-  async run() {},
+  async run() {
+    const vpc = new sst.aws.Vpc("MyVpc", { bastion: true, nat: "ec2" });
+    const rds = new sst.aws.Postgres("MyPostgres", { vpc, proxy: true });
+
+    new sst.x.DevCommand("Studio", {
+      link: [rds],
+      dev: {
+        command: "npx drizzle-kit studio",
+      },
+    });
+
+    new sst.aws.Function("MyApi", {
+      vpc,
+      url: true,
+      link: [rds],
+      handler: "src/api.handler",
+    });
+  },
 });
